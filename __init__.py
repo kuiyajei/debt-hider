d4e5d590dc1018fe021fd4c6134b3de813ee0e38
kuiyajei <60958090+kuiyajei@users.noreply.github.com>
2025-04-05 08:43:10 +0800

    content change
    - replaced emojis
    - changed meta.json

--- __init__.py
+++ __init__.py
@@ -160,25 +160,28 @@ from .adjust_css import *
 # Much as I wanted to put a "complete" symbol, all data with zero-count share the same class name.
 override_due_text = """
 <script>
-    var dues = document.querySelectorAll(".review-count"); 
-    for (let x of dues) {
-        x.textContent = "⏳";
-    } 
-    var finished = document.querySelectorAll(".zero-count"); 
-    for (let x of finished) {
-        x.textContent = "☑";
-    } 
+    document.addEventListener("DOMContentLoaded", function() {
+        var dues = document.querySelectorAll(".review-count"); 
+        for (let x of dues) {
+            x.textContent = "!";
+            x.style.color = "yellow"
+        } 
+        var finished = document.querySelectorAll(".zero-count"); 
+        for (let x of finished) {
+            x.textContent = "☑";
+        } 
+    });
 </script>
 """
 
-def deck_browser_hide(deck_browser, content):
+def inject_deck_browser_hide(deck_browser, content):
     # content.tree += f"{content.tree}"
     # original = """<span class="review-count">"""
     # new = """<span>📑</span><span class="review-count" hidden>"""
     # content.tree = content.tree.replace(original, new)
     content.tree += override_due_text
-gui_hooks.deck_browser_will_render_content.append(deck_browser_hide)
+gui_hooks.deck_browser_will_render_content.append(inject_deck_browser_hide)
 
-def deck_overview_hide(overview, content):
+def inject_overview_hide(overview, content):
     content.table += override_due_text
-gui_hooks.overview_will_render_content.append(deck_overview_hide)
+gui_hooks.overview_will_render_content.append(inject_overview_hide)
